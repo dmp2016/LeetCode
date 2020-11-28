0fe74d50c0c9bbfe2f12e8ae18c82cc89357ebfc
dmp <pashutkindv@yandex.ru>
2020-11-28 21:15:25 +0300

    -

---
+++ Merge Sorted Array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+
+test = Solution()
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+test.merge(nums1, 3, nums2, 3)
+
