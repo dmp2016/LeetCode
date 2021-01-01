d3d6339069d46249aed02dbd05e1af0b0aab017a
dmp2016 <pashutkindv@yandex.ru>
2021-01-01 17:41:36 +0300

    Variant with Priority queue

--- Maximum Frequency Stack.py
+++ Maximum Frequency Stack.py
@@ -1,36 +1,26 @@
-from sortedcontainers import SortedList
+from queue import PriorityQueue
+from heapq import heappush, heappop
+from collections import defaultdict
 
 
 class FreqStack:
 
     def __init__(self):
-        self.orderer = SortedList()
-        self.elem_nums = {}
+        self.orderer = PriorityQueue()
+        self.elem_nums = defaultdict(int)
         self.stack_cnt = 0
 
     def push(self, x: int) -> None:
         self.stack_cnt += 1
-        if x in self.elem_nums:
-            elem = (len(self.elem_nums[x]), self.elem_nums[x][-1], x)
-            self.orderer.remove(elem)
-            self.elem_nums[x].append(self.stack_cnt)
-            elem = (len(self.elem_nums[x]), self.elem_nums[x][-1], x)
-            self.orderer.add(elem)
-        else:
-            self.elem_nums[x] = [self.stack_cnt]
-            elem = (len(self.elem_nums[x]), self.elem_nums[x][-1], x)
-            self.orderer.add(elem)
+        self.elem_nums[x] += 1
+        elem = (-self.elem_nums[x], -self.stack_cnt, x)
+        self.orderer.put(elem)
 
     def pop(self) -> int:
-        elem = self.orderer.pop()
-        x = elem[2]
-        self.elem_nums[x].pop()
-        if len(self.elem_nums[x]) == 0:
-            self.elem_nums.pop(x)
-        else:
-            self.orderer.add((len(self.elem_nums[x]), self.elem_nums[x][-1], x))
+        _, _, x = self.orderer.get()
+        self.elem_nums[x] -= 1
         return x
-        
+
 
 test = FreqStack()
 test.push(5)
@@ -39,6 +29,7 @@ test.push(5)
 test.push(7)
 test.push(4)
 test.push(5)
+print(test.pop())
 print(test.pop())
 print(test.pop())
 print(test.pop())
