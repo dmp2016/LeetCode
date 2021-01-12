88e86e49a5e9d63cd07b5b0587fe9f8b64772c09
Dmitry <pashutkindv@yandex.ru>
2021-01-12 23:55:49 +0300

    -

--- Next Greater Element I.py
+++ Next Greater Element I.py
@@ -22,7 +22,6 @@ class Solution:
         return [res.get(m, -1) for m in nums1]
 
 
-
 test = Solution()
 print(test.nextGreaterElement(nums1=[4, 1, 2], nums2=[1, 3, 4, 2]))
 print(test.nextGreaterElement(nums1=[2, 4], nums2=[1, 2, 3, 4]))
