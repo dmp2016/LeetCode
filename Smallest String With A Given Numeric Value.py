092ad9c4a46fc50f64a9222fc8ebfcff74ca22ed
dmp2016 <pashutkindv@yandex.ru>
2021-01-30 11:49:00 +0300

    -

--- Smallest String With A Given Numeric Value.py
+++ Smallest String With A Given Numeric Value.py
@@ -1,5 +1,6 @@
 class Solution:
-    def getSmallestString(self, n: int, k: int) -> str:
+    # Slow
+    def getSmallestString1(self, n: int, k: int) -> str:
         res = ''
         while n > 0:
             n -= 1
@@ -9,8 +10,20 @@ class Solution:
             res += chr(c + ord('a') - 1)
         return res
 
+    def getSmallestString(self, n: int, k: int) -> str:
+        if (k - n) % 25 == 0:
+            c = (k - n) // 25
+            a = n - c
+            return 'a' * a + 'z' * c
+        else:
+            c = (k - n) // 25
+            b = k - n - 25 * c + 1
+            a = k - b - 26 * c
+            return 'a' * a + chr(ord('a') + b - 1) + 'z' * c
+
 
 test = Solution()
 print(test.getSmallestString(n=3, k=27))
 print(test.getSmallestString(n=5, k=73))
+print(test.getSmallestString(n=3, k=26*3))
 test.getSmallestString(n=10**5, k=10*10**5)
