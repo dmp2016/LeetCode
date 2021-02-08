ac546ed83463f19e0863866a1748e743f5e61b80
dmp2016 <pashutkindv@yandex.ru>
2021-02-08 19:54:54 +0300

    -

--- Maximum Sum Circular Subarray.py
+++ Maximum Sum Circular Subarray.py
@@ -1,26 +1,41 @@
 from typing import List
+from heapq import heappush, heappop
+from sortedcontainers import SortedList
 import math
 
 
 class Solution:
     def maxSubarraySumCircular(self, A: List[int]) -> int:
-        left, right = 0, 0
-        cur_sum = 0
-        max_sum = -math.inf
-        A = 2 * A
-        while right < len(A):
-            cur_sum += A[right]
-            max_sum = max(cur_sum, max_sum)
-            while cur_sum <= 0 and left < right:
-                cur_sum -= A[left]
-                left += 1
-            max_sum = max(cur_sum, max_sum)
-            right += 1
-        return max_sum
+        left_part = SortedList([0])
+        sum_left = 0
+        sum_right = A[0]
+        cur_max = sum_right - left_part[0]
+        for ind in range(1, len(A)):
+            sum_left += A[ind - 1]
+            sum_right += A[ind]
+            left_part.add(sum_left)
+            cur_max = max(cur_max, sum_right - left_part[0])
+        d_sum_left = 0
+        left_part.remove(d_sum_left)
+        sum_left += A[len(A) - 1]
+        sum_right += A[0]
+        if left_part:
+            cur_max = max(cur_max, sum_right - left_part[0])
+        for ind in range(1, len(A)):
+            d_sum_left += A[ind - 1]
+            sum_left += A[ind - 1]
+            sum_right += A[ind]
+            left_part.remove(d_sum_left)
+            left_part.add(sum_left)
+            if left_part:
+                cur_max = max(cur_max, sum_right - left_part[0])
+        return cur_max
 
 
 test = Solution()
-print(test.maxSubarraySumCircular([1, -2, 3, -2]))
-print(test.maxSubarraySumCircular([5, -3, 5]))
-print(test.maxSubarraySumCircular([3, -1, 2, -1]))
-print(test.maxSubarraySumCircular([-2, -3, -1]))
+# print(test.maxSubarraySumCircular([1, -2, 3, -2]))
+# print(test.maxSubarraySumCircular([5, -3, 5]))
+# print(test.maxSubarraySumCircular([3, -1, 2, -1]))
+# print(test.maxSubarraySumCircular([3, -2, 2, -3]))
+# print(test.maxSubarraySumCircular([-2, -3, -1]))
+print(test.maxSubarraySumCircular([-2]))
