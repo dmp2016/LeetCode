2b531a0771647b64db5588687ef81d6b337c9778
dmp2016 <pashutkindv@yandex.ru>
2021-02-21 17:19:41 +0300

    -

---
+++ Broken Calculator.py
@@ -0,0 +1,16 @@
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int:
+        if X >= Y:
+            return X - Y
+        else:
+            if Y & 1:
+                return 2 + self.brokenCalc(X, (Y + 1) // 2)
+            else:
+                return 1 + self.brokenCalc(X, Y // 2)
+
+
+test = Solution()
+print(test.brokenCalc(X=2, Y=3))
+print(test.brokenCalc(X=5, Y=8))
+print(test.brokenCalc(X=3, Y=10))
+print(test.brokenCalc(X=1024, Y=1))
