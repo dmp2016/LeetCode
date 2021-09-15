88f150a8f29afa6efc90f0b0544440a19922cb44
Dmitry <pashutkindv@yandex.ru>
2021-09-15 23:56:26 +0300

    -

--- Longest Turbulent Subarray.py
+++ Longest Turbulent Subarray.py
@@ -3,34 +3,18 @@ from typing import List
 
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        k = 0
-        ind2 = 1
-        ind1 = 0
-        cur_comp = None
+        sa = [arr[ind - 1] - arr[ind] for ind in range(1, len(arr))]
         res = 1
-        while ind2 < len(arr):
-            if arr[ind2 - 1] > arr[ind2]:
-                if cur_comp or cur_comp is None:
-                    cur_comp = False
-                else:
-                    res = max(res, ind2 - ind1)
-                    cur_comp = False
-                    ind1 = ind2 - 1
-            elif arr[ind2 - 1] < arr[ind2]:
-                if not cur_comp or cur_comp is None:
-                    cur_comp = True
-                else:
-                    res = max(res, ind2 - ind1)
-                    cur_comp = False
-                    ind1 = ind2 - 1
-                cur_comp = True
+        ind = 0
+        while ind < len(sa):
+            if sa[ind] != 0:
+                i = ind + 1
+                while i < len(sa) and (sa[i] > 0 and sa[i - 1] < 0 or sa[i] < 0 and sa[i - 1] > 0):
+                    i += 1
+                res = max(res, i - ind + 1)
+                ind = i
             else:
-                res = max(res, ind2 - ind1)
-                cur_comp = True
-                ind1 = ind2
-                cur_comp = None
-            ind2 += 1
-        res = max(res, ind2 - ind1)
+                ind += 1
         return res
 
 
