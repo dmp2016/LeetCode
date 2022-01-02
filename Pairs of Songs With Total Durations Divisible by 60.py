525ce7c1f8cf0e45d7f13f25e1a1be1d9a542b7d
Dmitry <pashutkindv@yandex.ru>
2022-01-02 15:23:05 +0300

    -

--- Pairs of Songs With Total Durations Divisible by 60.py
+++ Pairs of Songs With Total Durations Divisible by 60.py
@@ -1,7 +1,8 @@
 from typing import List
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 
+# Best!!!
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         r = defaultdict(int)
@@ -10,6 +11,25 @@ class Solution:
         res = 0
         for a in time:
             res += r[(-a) % 60]
+        res -= r[0] + r[30]
+        res >>= 1
+        return res
+
+
+# Slow !
+class Solution1:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        r = defaultdict(int)
+        for a in time:
+            r[a % 60] += 1
+        return (sum(r[(-a) % 60] for a in time) - (r[0] + r[30])) >> 1
+
+
+# Slow!
+class Solution2:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        r = Counter(a % 60 for a in time)
+        res = sum(r[(-a) % 60] for a in time)
         res -= r[0] + r[30]
         res >>= 1
         return res
