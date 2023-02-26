042213d39cadcfdc36c7bc426997f79f9de048ce
dmp2016 <pashutkindv@yandex.ru>
2023-02-26 14:26:53 +0300

    -

---
+++ Edit Distance.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        @lru_cache(None)
+        def rec(a: int, b: int) -> int:
+            if a == -1:
+                return b + 1
+            if b == -1:
+                return a + 1
+
+            return min([rec(a - 1, b) + 1, rec(a, b - 1) + 1, rec(a - 1, b - 1) + int(word1[a] != word2[b])])
+
+        return rec(len(word1) - 1, len(word2) - 1)
+
+
+test = Solution()
+print(test.minDistance(word1="horse", word2="ros"))
+print(test.minDistance(word1="intention", word2="execution"))
+print(test.minDistance(word1="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdef",
+                       word2="bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg"))
