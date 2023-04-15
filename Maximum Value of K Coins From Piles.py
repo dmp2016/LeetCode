ca0ae3143df3db33af61e630fffdadef9934c0fc
dmp2016 <pashutkindv@yandex.ru>
2023-04-15 23:08:13 +0300

    -

---
+++ Maximum Value of K Coins From Piles.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        pass
+
+
+t = Solution()
+print(t.maxValueOfCoins(piles=[[1, 100, 3], [7, 8, 9]], k=2))
+print(t.maxValueOfCoins(piles=[[100], [100], [100], [
+      100], [100], [100], [1, 1, 1, 1, 1, 1, 700]], k=7))
