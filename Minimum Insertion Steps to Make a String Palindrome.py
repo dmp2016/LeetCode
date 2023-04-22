26639270f4375605f915ad77c21e1a3ccc87bf7c
dmp2016 <pashutkindv@yandex.ru>
2023-04-22 20:54:58 +0300

    -

---
+++ Minimum Insertion Steps to Make a String Palindrome.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        
+        @lru_cache(None)
+        def do_rec(left: int, right: int) -> int:
+            if right - left <= 0:
+                return 0
+            if s[left] == s[right]:
+                return do_rec(left + 1, right - 1)
+            else:
+                return min(do_rec(left, right - 1), do_rec(left + 1, right)) + 1
+        
+        return do_rec(0, len(s) - 1)
+
+
+t = Solution()
+# print(t.minInsertions(s="zzazz"))
+# print(t.minInsertions(s="mbadm"))
+# print(t.minInsertions(s="leetcode"))
+print(t.minInsertions(s="tldjbqjdogipebqsohdypcxjqkrqltpgviqtqz"))
+print(t.minInsertions(s="hroamwegvonydrclqxwddrwiejzqcokfwcbsqourfexoylnhookppmirgwfowjpirgbcvlwrxtqncxvjqurjxzzxiknmwzsuolfskosoopmtzwgzsoxcdicsvkyuowhicfdadqekagqtaggwxfkucyhoonrqtrgbhtvpidmvvnzzjbgneoeaumvgikqbndxvpsxgddnuvrwvzvhrtkuazoeppejxggpwpuzaxcdghmwvdawqyqmhtapocsshvfhpofclsrmtkgia"))
